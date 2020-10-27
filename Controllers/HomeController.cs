9223702123d38bff115e35b8c3f9410a5ed11c50
StephenBrooks220413 <stephen.brooks220413@gmail.com>
2020-10-27 16:56:48 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CoffeeShop.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CoffeeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Menu()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Item1()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Item2()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Item3()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Item4()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Profile()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function info() {
+    document.getElementById("info").innerHTML = "Chicago, New York, Dallas, San Antonio, New Orleans, Atlanta";
+}
+
+function reset() {
+    document.getElementById("info").innerHTML = "Locations Shown Here";
+}
